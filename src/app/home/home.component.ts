f839882bac27ebb3538bbd49afadc48d9d51c649
nHoedl <nicholas.hoedl@ndsu.edu>
2025-03-13 18:49:38 -0500

    Added HousingLocation Interface and created a single instance of the new interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,15 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 })
 export class HomeComponent {
 
+  readonly baseURL = "https://angular.dev/assets/images/tutorials/common";
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: "Test Home",
+    city: "Test City",
+    state: "ST",
+    photo: `${this.baseURL}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
